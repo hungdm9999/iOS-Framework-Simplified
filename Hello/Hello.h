59270725d8ba2590ff098027ca7e5d271e354594
Do Manh Hung <hungdm9999@xmobilevn.com>
2014-07-11 07:16:31 +0700

    Initial Commit

---
+++ Hello/Hello.h
@@ -0,0 +1,13 @@
+//
+//  Hello.h
+//  Hello
+//
+//  Created by Do Manh Hung on 7/11/14.
+//  Copyright (c) 2014 XMobileVN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hello : NSObject
+
+@end
---
+++ Hello/Hello.m
@@ -0,0 +1,13 @@
+//
+//  Hello.m
+//  Hello
+//
+//  Created by Do Manh Hung on 7/11/14.
+//  Copyright (c) 2014 XMobileVN. All rights reserved.
+//
+
+#import "Hello.h"
+
+@implementation Hello
+
+@end
