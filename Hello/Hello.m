11fcc5ae05f1e436e9b795e54e3d4393898c591f
Do Manh Hung <hungdm9999@xmobilevn.com>
2014-07-11 07:29:02 +0700

    first init. See the Output console of Xcode when run the demo.

--- Hello/Hello.m
+++ Hello/Hello.m
@@ -9,5 +9,10 @@
 #import "Hello.h"
 
 @implementation Hello
-
++(void) helloFramework{
+    NSLog(@"Hello framework");
+    NSLog(@"Navigate to the Hello-Framework target, open the Run scrip in build phase and check it out.");
+    NSLog(@"Pay attention on the ${LIB_TARGET} variable, which is target to build lib${LIB_TARGET}.a");
+    NSLog(@"Both .framework and universal .a library was created. See the lib_output directory.");
+}
 @end
